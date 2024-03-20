ff0f494eeacd3366ed2a3d7418406fc986670106
RishabhSingh4872962 <109566428+RishabhSingh4872962@users.noreply.github.com>
2024-03-20 20:07:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //your JS code here. If required.
 const output=document.getElementById("output");
 function arr() {
-	return new Promise((resolve,reject)=>{
+	return new Promise((resolve,reject)=>{ 
 		setTimeout(resolve,3000,[1,2,3,4])
 	})
 } 
 arr().then((val)=>{
-	output.innerText=val
+	output.innerText=val.join(" ")
 	return new Promise((res,rej)=>{
 		setTimeout(res,1000,val.filter((ele)=>ele%2==0))
 	})
 }).then((val)=>{
-	output.innerText=val
+	output.innerText=val.join(" ")
 	setTimeout(()=>{
-		output.innerText=(val.map((ele)=>ele*2))
+		output.innerText=(val.map((ele)=>ele*2)).join(" ");
 	},2000)
 })
